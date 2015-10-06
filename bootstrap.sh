a9861e2859b286d69e6e96eca9b4167d3dc5ba5c
Dylan Wood <dwood@mrn.org>
2015-10-05 20:08:43 -0700

    site.yml->playbook.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,5 +16,5 @@ fi
 echo 'Running Ansible to configure Dev machine'
 
 if [[ -x `which ansible` ]]; then
-    ansible-playbook -i hosts site.yml --ask-sudo-pass
+    ansible-playbook -i hosts playbook.yml --ask-sudo-pass
 fi
